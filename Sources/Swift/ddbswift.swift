e039b0c1b17f06788d8421a4f341c2969570b0cc
Nicolai Syvertsen <saivert@saivert.com>
2023-01-14 23:13:01 +0100

    format with swift-format

--- Sources/Swift/ddbswift.swift
+++ Sources/Swift/ddbswift.swift
@@ -1,6 +1,6 @@
+import Foundation
 @_exported import Glibc
 @_exported import ddbswift_C
-import Foundation
 
 func start() -> Int32 {
     0
@@ -15,12 +15,12 @@ func message(id: UInt32, ctx: UInt, p1: UInt32, p2: UInt32) -> Int32 {
 }
 
 enum Constants {
-static let id = NSString("someid")
-static let name = NSString("Swift DDB")
-static let descr = NSString("swift!!")
-static let copyright = NSString("gpl")
-static let x: StaticString = "https://saivert.com/"
-static let prefix: NSString = NSString("tone")
+    static let id = NSString("someid")
+    static let name = NSString("Swift DDB")
+    static let descr = NSString("swift!!")
+    static let copyright = NSString("gpl")
+    static let x: StaticString = "https://saivert.com/"
+    static let prefix: NSString = NSString("tone")
 }
 
 var plug = DB_plugin_t(
@@ -41,10 +41,10 @@ var plug = DB_plugin_t(
     command: nil,
     start: start,
     stop: stop,
-    connect: {() -> Int32 in 
-            print("Swift: Connect called!")
-            return 0
-        },
+    connect: { () -> Int32 in
+        print("Swift: Connect called!")
+        return 0
+    },
     disconnect: nil,
     exec_cmdline: nil,
     get_actions: nil,
@@ -52,9 +52,9 @@ var plug = DB_plugin_t(
     configdialog: nil
 )
 
-
-func allocToneFileInfo() -> UnsafeMutablePointer<toneFileInfo>  {
-    let ptr: UnsafeMutablePointer<toneFileInfo> = UnsafeMutablePointer<toneFileInfo>.allocate(capacity: 1)
+func allocToneFileInfo() -> UnsafeMutablePointer<toneFileInfo> {
+    let ptr: UnsafeMutablePointer<toneFileInfo> = UnsafeMutablePointer<toneFileInfo>.allocate(
+        capacity: 1)
     ptr.initialize(to: toneFileInfo())
     ptr.pointee.file = nil
     ptr.pointee.info.file = nil
@@ -62,7 +62,9 @@ func allocToneFileInfo() -> UnsafeMutablePointer<toneFileInfo>  {
     return ptr
 }
 
-func read(info: UnsafeMutablePointer<DB_fileinfo_t>?, bytes: UnsafeMutablePointer<CChar>?, size: Int32) -> Int32 {
+func read(
+    info: UnsafeMutablePointer<DB_fileinfo_t>?, bytes: UnsafeMutablePointer<CChar>?, size: Int32
+) -> Int32 {
     // print("swift: read")
     let toneinfo = UnsafeMutablePointer<toneFileInfo>(OpaquePointer(info))!
 
@@ -71,19 +73,21 @@ func read(info: UnsafeMutablePointer<DB_fileinfo_t>?, bytes: UnsafeMutablePointe
     }
 
     let freq = Float(toneinfo.pointee.frequency)
-    let cap = Int(size)/MemoryLayout<Float>.size
+    let cap = Int(size) / MemoryLayout<Float>.size
 
-    bytes!.withMemoryRebound(to: Float.self, capacity: cap, {(a) in
-        for sample in 0 ..< cap {
+    bytes!.withMemoryRebound(
+        to: Float.self, capacity: cap,
+        { (a) in
+            for sample in 0..<cap {
 
-            let samplevalue = sin(toneinfo.pointee.m_phase * Float.pi * 2)
-            toneinfo.pointee.m_phase += freq / 48000 / 2;
-            toneinfo.pointee.m_phase -= floor(toneinfo.pointee.m_phase);
+                let samplevalue = sin(toneinfo.pointee.m_phase * Float.pi * 2)
+                toneinfo.pointee.m_phase += freq / 48000 / 2
+                toneinfo.pointee.m_phase -= floor(toneinfo.pointee.m_phase)
 
-            a[sample] = Float(samplevalue * 0.5)
-            a[sample+1] = Float(samplevalue * 0.5)
-        }
-    })
+                a[sample] = Float(samplevalue * 0.5)
+                a[sample + 1] = Float(samplevalue * 0.5)
+            }
+        })
 
     toneinfo.pointee.info.readpos += 1
 
@@ -92,47 +96,47 @@ func read(info: UnsafeMutablePointer<DB_fileinfo_t>?, bytes: UnsafeMutablePointe
 
 var inputplg = DB_decoder_t(
     plugin: plug,
-    open: {(hints: UInt32) -> UnsafeMutablePointer<DB_fileinfo_t>? in
-            print("swift: open")
-            let ptr = allocToneFileInfo()
-            return UnsafeMutablePointer<DB_fileinfo_t>(OpaquePointer(ptr))
-        },
-    init: {(info: UnsafeMutablePointer<DB_fileinfo_t>?, it: UnsafeMutablePointer<DB_playItem_t>?) -> Int32 in
-            print("swift: init")
-            let toneinfo = UnsafeMutablePointer<toneFileInfo>(OpaquePointer(info))!
+    open: { (hints: UInt32) -> UnsafeMutablePointer<DB_fileinfo_t>? in
+        print("swift: open")
+        let ptr = allocToneFileInfo()
+        return UnsafeMutablePointer<DB_fileinfo_t>(OpaquePointer(ptr))
+    },
+    init: {
+        (info: UnsafeMutablePointer<DB_fileinfo_t>?, it: UnsafeMutablePointer<DB_playItem_t>?)
+            -> Int32 in
+        print("swift: init")
+        let toneinfo = UnsafeMutablePointer<toneFileInfo>(OpaquePointer(info))!
+
+        if toneinfo.pointee.file == nil {
+            deadbeef.pl_lock()
+            let uri = String(cString: deadbeef.pl_find_meta(it, ":URI")!)
+            deadbeef.pl_unlock()
+
+            toneinfo.pointee.file = deadbeef.fopen(uri)
+
+            toneinfo.pointee.frequency = deadbeef.pl_find_meta_int(it, "frequency", 440)
 
             if toneinfo.pointee.file == nil {
-                deadbeef.pl_lock ();
-                let uri = String(cString: deadbeef.pl_find_meta (it, ":URI")!)
-                deadbeef.pl_unlock ();
-
-                toneinfo.pointee.file = deadbeef.fopen(uri)
-
-                toneinfo.pointee.frequency = deadbeef.pl_find_meta_int(it, "frequency", 440)
-
-                if toneinfo.pointee.file == nil {
-                    print("failed to open file ", uri)
-                    return -1
-                }
+                print("failed to open file ", uri)
+                return -1
             }
+        }
 
+        toneinfo.pointee.info.plugin = UnsafeMutablePointer<DB_decoder_t>(&inputplg)
 
-            toneinfo.pointee.info.plugin = UnsafeMutablePointer<DB_decoder_t>(&inputplg)
+        toneinfo.pointee.info.readpos = 0
 
+        toneinfo.pointee.info.fmt = ddb_waveformat_t(
+            bps: 32,
+            channels: 2,
+            samplerate: 48000,
+            channelmask: 3,
+            is_float: 1,
+            is_bigendian: 0)
 
-            toneinfo.pointee.info.readpos = 0
-
-            toneinfo.pointee.info.fmt = ddb_waveformat_t(
-                bps: 32,
-                channels: 2,
-                samplerate: 48000,
-                channelmask: 3,
-                is_float: 1,
-                is_bigendian: 0)
-
-            return 0
-        },
-    free: {(info: UnsafeMutablePointer<DB_fileinfo_t>?) -> Void in
+        return 0
+    },
+    free: { (info: UnsafeMutablePointer<DB_fileinfo_t>?) -> Void in
         print("swift: free")
         if let toneinfo = UnsafeMutablePointer<toneFileInfo>(OpaquePointer(info)) {
             if toneinfo.pointee.file != nil {
@@ -141,68 +145,74 @@ var inputplg = DB_decoder_t(
             toneinfo.deinitialize(count: 1)
             toneinfo.deallocate()
         }
-     },
+    },
     read: read,
-    seek: {(info: UnsafeMutablePointer<DB_fileinfo_t>?, time: Float) -> Int32 in
-            print("swift: seek")
-            return -1
-        },
-    seek_sample: {(info: UnsafeMutablePointer<DB_fileinfo_t>?, sample: Int32) -> Int32 in
-            print("swift: seek sample")
-            return -1
-        },
-    insert: {(plt: UnsafeMutablePointer<ddb_playlist_t>?, after: UnsafeMutablePointer<DB_playItem_t>?, fname: UnsafePointer<CChar>?) -> UnsafeMutablePointer<DB_playItem_t>? in
-            let fname_string = fname.map{String(cString:$0)} ?? ""
-            print("swift: insert" + (fname.map{" " + String(cString:$0)} ?? ""))
+    seek: { (info: UnsafeMutablePointer<DB_fileinfo_t>?, time: Float) -> Int32 in
+        print("swift: seek")
+        return -1
+    },
+    seek_sample: { (info: UnsafeMutablePointer<DB_fileinfo_t>?, sample: Int32) -> Int32 in
+        print("swift: seek sample")
+        return -1
+    },
+    insert: {
+        (
+            plt: UnsafeMutablePointer<ddb_playlist_t>?, after: UnsafeMutablePointer<DB_playItem_t>?,
+            fname: UnsafePointer<CChar>?
+        ) -> UnsafeMutablePointer<DB_playItem_t>? in
+        let fname_string = fname.map { String(cString: $0) } ?? ""
+        print("swift: insert" + (fname.map { " " + String(cString: $0) } ?? ""))
 
-            do {
-                var contents = try String(contentsOfFile: fname_string, encoding: String.Encoding.utf8)
-                let freq = contents.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
+        do {
+            var contents = try String(contentsOfFile: fname_string, encoding: String.Encoding.utf8)
+            let freq = contents.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
 
-                let it = deadbeef.pl_item_alloc_init(fname, plug.id)!
-                // deadbeef.pl_add_meta (it, ":FILE_SIZE", "10000")
-                deadbeef.pl_add_meta (it, ":FILETYPE", "tone")
-                deadbeef.pl_add_meta (it, ":CHANNELS", "2")
-                deadbeef.pl_add_meta (it, ":BPS", "16")
-                deadbeef.pl_add_meta (it, ":SAMPLERATE", "48000")
-                deadbeef.pl_add_meta (it, "artist", "the tone generators")
-                deadbeef.pl_add_meta (it, "title", "Frequency \(freq)")
-                deadbeef.pl_add_meta (it, "frequency", freq)
-                // deadbeef.pl_set_meta_int (it, ":BITRATE", 705600)
-                let after =  deadbeef.plt_insert_item (plt, after, it)
-                deadbeef.pl_item_unref(it)
-                return after
+            let it = deadbeef.pl_item_alloc_init(fname, plug.id)!
+            // deadbeef.pl_add_meta (it, ":FILE_SIZE", "10000")
+            deadbeef.pl_add_meta(it, ":FILETYPE", "tone")
+            deadbeef.pl_add_meta(it, ":CHANNELS", "2")
+            deadbeef.pl_add_meta(it, ":BPS", "16")
+            deadbeef.pl_add_meta(it, ":SAMPLERATE", "48000")
+            deadbeef.pl_add_meta(it, "artist", "the tone generators")
+            deadbeef.pl_add_meta(it, "title", "Frequency \(freq)")
+            deadbeef.pl_add_meta(it, "frequency", freq)
+            // deadbeef.pl_set_meta_int (it, ":BITRATE", 705600)
+            let after = deadbeef.plt_insert_item(plt, after, it)
+            deadbeef.pl_item_unref(it)
+            return after
 
-            } catch {
-                return nil
-            }
+        } catch {
+            return nil
+        }
 
-        },
+    },
     numvoices: nil,
     mutevoice: nil,
-    read_metadata: {(it: UnsafeMutablePointer<DB_playItem_t>?) -> Int32 in
-            print("swift: read metadata")
-            return 0
-        },
-    write_metadata: {(it: UnsafeMutablePointer<DB_playItem_t>?) -> Int32 in
-            print("swift: write metadata")
-            return 0
-        },
+    read_metadata: { (it: UnsafeMutablePointer<DB_playItem_t>?) -> Int32 in
+        print("swift: read metadata")
+        return 0
+    },
+    write_metadata: { (it: UnsafeMutablePointer<DB_playItem_t>?) -> Int32 in
+        print("swift: write metadata")
+        return 0
+    },
     exts: nil,
     prefixes: nil,
     open2: nil
-    // open2: {(hints: UInt32, it: UnsafeMutablePointer<DB_playItem_t>?) -> UnsafeMutablePointer<DB_fileinfo_t>? in
-    //         print("swift: open2")
-    //         let ptr = allocToneFileInfo()
-    //         return UnsafeMutablePointer(OpaquePointer(ptr))
-    //     }
-    )
+        // open2: {(hints: UInt32, it: UnsafeMutablePointer<DB_playItem_t>?) -> UnsafeMutablePointer<DB_fileinfo_t>? in
+        //         print("swift: open2")
+        //         let ptr = allocToneFileInfo()
+        //         return UnsafeMutablePointer(OpaquePointer(ptr))
+        //     }
+)
 
-var deadbeef : DB_functions_t!
+var deadbeef: DB_functions_t!
 
 var exts = UnsafeMutablePointer<UnsafePointer<CChar>?>.allocate(capacity: 2)
 
-@_cdecl("libddbswift_load") func libddbswift_load(api: UnsafePointer<DB_functions_t>) -> UnsafeMutablePointer<DB_decoder_t> {
+@_cdecl("libddbswift_load") func libddbswift_load(api: UnsafePointer<DB_functions_t>)
+    -> UnsafeMutablePointer<DB_decoder_t>
+{
 
     deadbeef = api.pointee
 
@@ -212,5 +222,3 @@ var exts = UnsafeMutablePointer<UnsafePointer<CChar>?>.allocate(capacity: 2)
 
     return UnsafeMutablePointer<DB_decoder_t>(&inputplg)
 }
-
-
